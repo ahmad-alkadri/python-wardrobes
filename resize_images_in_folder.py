7661af408ed13d51977e54715a654cd4785c41d4
Ahmad Alkadri <ahmad.alkadri@outlook.com>
2020-01-26 11:39:26 +0100

    Added description and how to use

--- resize_images_in_folder.py
+++ resize_images_in_folder.py
@@ -1,6 +1,30 @@
 #### PYTHON CODE: RESIZE IMAGES IN A FOLDER ####
 #### Dependances: PIL, os, sys, glob
 
+#### DESCRIPTION
+#### Like its name implies, this code is useful for resizing many images
+#### in a particular folder all at once. The idea is having a code that 
+#### you can launch once and resize all the images based on their width
+#### and keeping their aspect ratio. Be careful: the saved image will have
+#### the same file name as its original one. Essentially, it will replace
+#### the original file with the resized one.
+
+#### HOW TO USE
+#### This code depends on four Python modules: PIL, os, sys, and glob.
+#### All of them are installable through conda or pip (and normally, 
+#### if you install your Python through distributions such as Anaconda,
+#### those packages are already installed. If not, please install them first.
+#### If those packages are installed already, just put this code in the same 
+#### folder where your original images are residing, then launch it.
+
+#### CUSTOMIZATION
+#### 1) By default, the image types that will be converted are PNG, JPG, and JPEG
+####    (see line 35). You can of course add more image extensions such as TIF.
+#### 2) By default, the final width is 1200 (see line 32). You can always
+####    modify it.
+#### 3) All images with width size already smaller than finalwidth will not
+####    be modified.
+
 from PIL import Image
 import os, sys, glob
 
